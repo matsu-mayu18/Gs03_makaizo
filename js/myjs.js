14c028a25211e6f968c4fec48ea6d54020e85387
matsu-mayu18 <rishinryu18vvm.m@gmail.com>
2024-05-16 09:36:41 -0700

    commited on 051624

---
+++ js/myjs.js
@@ -0,0 +1,15 @@
+// function drawVideo() {
+//   var video = document.getElementById("video_0");
+//   var canvas = document.getElementById("canvas_0");
+
+//   canvas.getContext("2d").drawImage(video, 0, 0, 250, 360);
+// }
+
+// function realtimeVideo() {
+//   var video = document.getElementById("video_0");
+
+//   setInterval(function () {
+//     var canvas = document.getElementById("canvas_0");
+//     canvas.getContext("2d").drawImage(video, 0, 0, 250, 360);
+//   }, 1000 / 30);
+// }
